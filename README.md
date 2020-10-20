fea26ead3da707129f25cdb728b4de73fb5c35af
Khoi Tran <ktran@sandbox.com>
2020-10-20 13:54:35 -0700

    init
    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# JohnFajardo: The website
+A third attempt at a personal website
+
+## The problem
+As a recent bootcamp grad, I need a personal website, but I'm totally broke. I could use GitHub pages, but where's the fun in that? I tried Jekyll on Namecheap but the experience is so clunky it's almost like writing a complete website by hand on each update. So with a Namecheap account and $10 to my name, I decided to make the best with what I have:
+
+* Heroku's free tier goes to sleep after half an hour on inactivity. I want potential employers to load the site FAST.
+* Rails+React is a big nono on shared plans.
+* Database is limited to MySQL.
+* I already have a domain name and a Namecheap account.
+* Rails on Namecheap is not possible on their shared plan.
+
+So what CAN I do on a shared plan?
+* I can do php.
+* I can do MySQL.
+* I can use my own domain.
+
+So that leaves one viable candidate: Wordpress. Love it or hate it, it's here to stay, it powers a huge portion of the internet and I had a kick with it back in college doing crappy websites for local businesses. Besides, I hadn't touched php in years so this will be fun. I also just about had it with Bootstrap, so this time I'm only using plain old SCSS.
+
+This would be v3 of my website. There is a previously unpublished V2 running perfectly fine on my box, but it uses Bootstrap so I've never been happy with it.
---
+++ gulpfile.babel.js
@@ -0,0 +1,68 @@
+import gulp from 'gulp';
+import sass from 'gulp-sass';
+var sourcemaps = require('gulp-sourcemaps');
+import cleanCSS from 'gulp-clean-css';
+import imagemin from "gulp-imagemin";
+import browserSync from "browser-sync";
+
+const sassOptions = {outputStyle: 'expanded', errLogToConsole: true};
+
+exports.sass = () => (
+    gulp.src('./src/scss/**/*.scss')
+    .pipe(sass(sassOptions))
+    .pipe(sourcemaps.init())
+		.pipe(sass({
+			outputStyle: 'expanded',
+			sourceComments: true
+		}).on("error", function(error){
+			console.log(error);
+			console.log("\n******** SASS COMPILE ERROR - check the above SASS output for errors\n\n");
+			beep();
+			sass.logError();
+		}))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(browserSync.reload({stream: true}))
+);
+
+exports.images = () => (
+    gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'))
+    .pipe(browserSync.reload({stream: true}))
+);
+
+exports.copy = () => (
+    gulp.src('./src/*.html')
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.reload({stream: true}))
+);
+
+exports.minifycss = () => (
+    gulp.src('./dist/css/styles.css')
+    .pipe(cleanCSS({compatibility: 'ie8', level: 2}))
+    .pipe(gulp.dest('dist/css'))
+);
+
+exports.scripts = () => (
+    gulp.src('./src/js/*.js')
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(browserSync.reload({stream: true}))
+);
+
+gulp.task('serve', () => {
+    browserSync.init({
+        server: {
+            baseDir: './dist',
+            index: 'index.html' 
+        },
+        notify: false,
+        injectChanges: true
+    });
+    gulp.watch('./src/scss/**/*', gulp.series('sass'));
+    gulp.watch('./src/images/**/*', gulp.series('images'));
+    gulp.watch('./src/*.html', gulp.series('copy'));
+    gulp.watch('./src/js/*.js', gulp.series('scripts'));
+    gulp.watch('./dist/*').on('change', browserSync.reload);
+});
+
+gulp.task('default', gulp.series('serve'));
